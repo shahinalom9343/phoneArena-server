b64a43561d44ced7f645a73fe1a4f7b4225fc2b3
shahinalom9343 <cuet.shahin.eee12@gmail.com>
2024-08-16 07:43:45 -0700

    MongoDB setup done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// mongodb driver code
+const uri =
+  "mongodb+srv://phoneArena:eZPCi4vKtf8iIAyU@cluster0.43teffq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Phone Arena Server is Running");
